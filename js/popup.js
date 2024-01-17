2e1deb1ef5f9db05c13e6fec2d3473a76880c70c
GitErwann <130602953+GitErwann@users.noreply.github.com>
2024-01-17 11:41:57 +0100

    Import Portfolio Site

---
+++ js/popup.js
@@ -0,0 +1,21 @@
+
+function openVMware() {
+document.getElementById("popup-VMware").style.display = "block"; 
+}
+function closeVMware() {
+document.getElementById("popup-VMware").style.display = "none";
+}
+
+function openWindows() {
+    document.getElementById("popup-windows").style.display = "block"; 
+    }
+    function closeWindows() {
+    document.getElementById("popup-windows").style.display = "none";
+    }
+
+function openOffice() {
+document.getElementById("popup-office").style.display = "block"; 
+}
+function closeOffice() {
+document.getElementById("popup-office").style.display = "none";
+}
